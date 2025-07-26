e4c198e0e2afa3b3c81e0923391345dab2ec2123
DishaBasti <148947200+DishaBasti@users.noreply.github.com>
2025-07-26 22:12:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",         # for local dev
         "https://strokewise-6517a.web.app/",        # Firebase hosted
-        "https://strokewise-6517a.firebaseapp.com"]# alternate Firebase domain
+        "https://strokewise-6517a.firebaseapp.com"], # alternate Firebase domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
